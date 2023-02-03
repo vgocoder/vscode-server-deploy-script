eb729ae07abfa8b10af634e9df035bb1e62187e0
zhonghua <zhonghua@kingsmith.com.cn>
2023-02-03 13:37:29 +0800

    add log format

--- run-vscode-server.sh
+++ run-vscode-server.sh
@@ -5,23 +5,32 @@ vscodeCliPath="/usr/bin"
 vscodeLogFile="vscode-server.log"
 tunnelName=$1
 
+# functions
+
+log() {
+  logfile=$vscodeLogFile
+  timestamp=$(date +"%Y-%m-%d %T")
+  echo "$timestamp: $1" >> $logfile
+}
+
 # tunnel name
 
 if [ -z "$1" ]; then
     tunnelName="tunnel-`cat /proc/sys/kernel/random/uuid  | md5sum |cut -c 1-8`"
-    echo $tunnelName
+    log "tunnel name not specified,just specify a name at random:$tunnelName "
 fi
 
 # pid
-PIDS=`ps -ef | grep "{$vscodeCliFile} tunnel" | grep -v grep | awk '{print $2}'`
+PIDS=`ps -ef | grep "{$vscodeCliFile}" | grep -v grep | awk '{print $2}'`
 for pid in $PIDS
 do
   kill -9 $pid
+  log "$pid killed"
 done
 
 # vscode-server bin file
 if [ ! -f "$vscodeCliBinFile" ]; then
-    echo "$vscodeCliBinFile not found!"
+    log "$vscodeCliBinFile not found,exit now"
     exit 0
 fi 
 
@@ -32,7 +41,8 @@ if [ ! -f "$vscodeCliPath/$vscodeCliFile" ]; then
 fi 
 
 # start tunnel
-nohup $vscodeCliPath/$vscodeCliFile tunnel --name $tunnelName --accept-server-license-terms > $vscodeLogFile 2>&1 &
+nohup $vscodeCliPath/$vscodeCliFile tunnel prune 2>&1 | while read line; do log "$line" >> $vscodeLogFile; done &
+nohup $vscodeCliPath/$vscodeCliFile tunnel --name $tunnelName --accept-server-license-terms 2>&1 | while read line; do log "$line" >> $vscodeLogFile; done &
 
 # logs tail
 tail -f $vscodeLogFile
